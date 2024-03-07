52386f13e1637b0c87ceb05cd7c4b2ac528dc289
MarcosRillo <marcosrillocabanne@gmail.com>
2024-03-07 18:34:28 -0300

    imagenes importadas

--- Ecommerce/src/assets/imagenes.js
+++ Ecommerce/src/assets/imagenes.js
@@ -1,6 +1,27 @@
 import salmonImg from './img/salmon.jpg'
 import lomoImg from './img/lomo.jpg'
 import risottoImg from './img/risotto.jpg'
+import pechugaImg from './img/pechuga.jpg'
+import canelonesImg from './img/canelones.jpg'
+import lomoMostazaImg from './img/lomoMostaza.jpg'
+import ensaladaImg from './img/ensalada.jpg'
+import spaghettiImg from './img/spaghetti.jpg'
+import bruschettaImg from './img/bruschetta.jpg'
+import empanadasImg from './img/empanadas.jpg'
+import carpaccioImg from './img/carpaccio.jpg'
+import palitosImg from './img/palitos.jpg'
+import aguacateImg from './img/aguacate.jpg'
+import tartarImg from './img/tartar.jpg'
+import sopaImg from './img/sopa.jpg'
+import quesadillasImg from './img/quesadillas.jpg'
+import margaritaImg from './img/margarita.jpg'
+import cafeImg from './img/cafe.jpg'
+import limonadaImg from './img/limonada.jpg'
+import smoothieImg from './img/smoothie.jpg'
+import sodaImg from './img/soda.jpg'
+import teImg from './img/te.jpg'
+import cervezaImg from './img/cerveza.jpg'
+import zumoImg from './img/zumo.jpg'
 
 
 
@@ -9,6 +30,27 @@ import risottoImg from './img/risotto.jpg'
 
 
 
-
-
-export default {salmonImg ,lomoImg, risottoImg}
+export default {salmonImg,
+                lomoImg,
+                risottoImg,
+                pechugaImg,
+                canelonesImg,
+                lomoMostazaImg,
+                ensaladaImg,
+                spaghettiImg,
+                bruschettaImg,
+                empanadasImg,
+                carpaccioImg,
+                palitosImg,
+                aguacateImg,
+                tartarImg,
+                sopaImg,
+                quesadillasImg,
+                margaritaImg,
+                cafeImg,
+                limonadaImg,
+                smoothieImg,
+                sodaImg,
+                teImg,
+                cervezaImg,
+                zumoImg}
--- Ecommerce/src/asyncMock.js
+++ Ecommerce/src/asyncMock.js
@@ -28,6 +28,195 @@ const platos = [
         stock: 15,
         description: 'Risotto con hongos de pino'
     },
+    {
+        id: '4',
+        name: 'Pechuga de Pollo a la Parrilla',
+        price: 9500,
+        category: 'principales',
+        img: pictures.pechugaImg,
+        stock: 8,
+        description: 'Pechuga de pollo a la parrilla con salsa de hierbas'
+    },
+    {
+        id: '5',
+        name: 'Canelones de Carne',
+        price: 8000,
+        category: 'principales',
+        img: pictures.canelonesImg,
+        stock: 10,
+        description: 'Canelones rellenos de carne con salsa de tomate y queso'
+    },
+    {
+        id: '6',
+        name: 'Lomo a la Mostaza',
+        price: 12000,
+        category: 'principales',
+        img: pictures.lomoMostazaImg,
+        stock: 7,
+        description: 'Lomo con salsa de mostaza y acompañamiento de papas al horno'
+    },
+    {
+        id: '7',
+        name: 'Ensalada de Pollo Caesar',
+        price: 9000,
+        category: 'principales',
+        img: pictures.ensaladaImg,
+        stock: 12,
+        description: 'Ensalada fresca con pollo a la parrilla y aderezo Caesar'
+    },
+    {
+        id: '8',
+        name: 'Spaghetti Bolognese',
+        price: 8500,
+        category: 'principales',
+        img: pictures.spaghettiImg,
+        stock: 9,
+        description: 'Spaghetti con salsa boloñesa y queso parmesano'
+    },
+    {
+        id: '9',
+        name: 'Bruschetta Caprese',
+        price: 6500,
+        category: 'entradas',
+        img: pictures.bruschettaImg,
+        stock: 15,
+        description: 'Pan tostado con tomate, mozzarella y albahaca'
+    },
+    {
+        id: '10',
+        name: 'Empanadas de Queso y Jamón',
+        price: 7500,
+        category: 'entradas',
+        img: pictures.empanadasImg,
+        stock: 20,
+        description: 'Empanadas rellenas de queso y jamón'
+    },
+    {
+        id: '11',
+        name: 'Carpaccio de Res',
+        price: 9800,
+        category: 'entradas',
+        img: pictures.carpaccioImg,
+        stock: 12,
+        description: 'Finas láminas de res marinadas con aceite de oliva y parmesano'
+    },
+    {
+        id: '12',
+        name: 'Palitos de Mozzarella',
+        price: 7200,
+        category: 'entradas',
+        img: pictures.palitosImg,
+        stock: 18,
+        description: 'Palitos de mozzarella empanizados con salsa de tomate'
+    },
+    {
+        id: '13',
+        name: 'Aguacate Relleno de Cangrejo',
+        price: 8900,
+        category: 'entradas',
+        img: pictures.aguacateImg,
+        stock: 10,
+        description: 'Aguacate relleno de carne de cangrejo y aderezo especial'
+    },
+    {
+        id: '14',
+        name: 'Tartar de Atún',
+        price: 11000,
+        category: 'entradas',
+        img: pictures.tartarImg,
+        stock: 8,
+        description: 'Tartar fresco de atún con aguacate y salsa de soja'
+    },
+    {
+        id: '15',
+        name: 'Sopa de Calabaza',
+        price: 6800,
+        category: 'entradas',
+        img: pictures.sopaImg,
+        stock: 14,
+        description: 'Sopa cremosa de calabaza con crutones'
+    },
+    {
+        id: '16',
+        name: 'Quesadillas de Pollo',
+        price: 8200,
+        category: 'entradas',
+        img: pictures.quesadillasImg,
+        stock: 16,
+        description: 'Quesadillas rellenas de pollo y queso derretido'
+    },
+    {
+        id: '17',
+        name: 'Margarita de Fresa',
+        price: 9500,
+        category: 'bebidas',
+        img: pictures.margaritaImg,
+        stock: 20,
+        description: 'Cóctel refrescante de margarita con sabor a fresa'
+    },
+    {
+        id: '18',
+        name: 'Café Frappé',
+        price: 7800,
+        category: 'bebidas',
+        img: pictures.cafeImg,
+        stock: 15,
+        description: 'Café helado con crema y jarabe de vainilla'
+    },
+    {
+        id: '19',
+        name: 'Limonada de Mango',
+        price: 7200,
+        category: 'bebidas',
+        img: pictures.limonadaImg,
+        stock: 18,
+        description: 'Limonada refrescante con un toque de mango'
+    },
+    {
+        id: '20',
+        name: 'Smoothie de Frutas',
+        price: 8500,
+        category: 'bebidas',
+        img: pictures.smoothieImg,
+        stock: 12,
+        description: 'Batido suave de frutas frescas y yogur'
+    },
+    {
+        id: '21',
+        name: 'Agua Mineral con Gas',
+        price: 5000,
+        category: 'bebidas',
+        img: pictures.sodaImg,
+        stock: 25,
+        description: 'Agua mineral con gas, perfecta para acompañar la comida'
+    },
+    {
+        id: '22',
+        name: 'Té Helado de Durazno',
+        price: 6800,
+        category: 'bebidas',
+        img: pictures.teImg,
+        stock: 14,
+        description: 'Té helado con sabor a durazno, endulzado y servido con hielo'
+    },
+    {
+        id: '23',
+        name: 'Cerveza Artesanal',
+        price: 9000,
+        category: 'bebidas',
+        img: pictures.cervezaImg,
+        stock: 10,
+        description: 'Cerveza artesanal de la casa, elaborada con ingredientes seleccionados'
+    },
+    {
+        id: '24',
+        name: 'Zumo de Naranja Fresco',
+        price: 7500,
+        category: 'bebidas',
+        img: pictures.zumoImg,
+        stock: 16,
+        description: 'Zumo de naranja recién exprimido, natural y lleno de vitaminas'
+    },
 ]
 
 export const getProducts = () => {
@@ -49,7 +238,7 @@ export const getProductsById = (productId) => {
 export const getProductsByCategory = (productCategory) => {
     return new Promise((resolve) => {
         setTimeout(() => {
-            resolve(platos.find(prod => prod.category === productCategory))
+            resolve(platos.filter(prod => prod.category === productCategory))
         }, 500);
     })
 }
