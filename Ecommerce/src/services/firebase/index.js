59ab8b22be09fbde4a1455bad1b23ecdc4bee143
MarcosRillo <marcosrillocabanne@gmail.com>
2024-04-18 18:41:45 -0300

    Reentrega Proyecto final Marcos-Rillo-Cabanne

--- Ecommerce/src/services/firebase/index.js
+++ Ecommerce/src/services/firebase/index.js
@@ -1,14 +1,14 @@
-
+import { apiKey, authDomain, projectId, storageBucket, messagingSenderId, appId } from './env';
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDehOw-yxV7KC4loLl7Rz_FBM2U5w9xT4k",
-  authDomain: "la-caballeriza.firebaseapp.com",
-  projectId: "la-caballeriza",
-  storageBucket: "la-caballeriza.appspot.com",
-  messagingSenderId: "761115719764",
-  appId: "1:761115719764:web:30f53791f3e2822b7015c1"
+  apiKey: apiKey,
+    authDomain: authDomain,
+    projectId: projectId,
+    storageBucket: storageBucket,
+    messagingSenderId: messagingSenderId,
+    appId: appId
 };
 
 const app = initializeApp(firebaseConfig);
